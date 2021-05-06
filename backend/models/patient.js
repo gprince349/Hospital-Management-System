61028973b6c9c1431070de61fb7dab8c0e795bc2
kaushal-cloud <kaushaluj1011@gmail.com>
2021-05-06 14:12:04 +0530

    some more changes in patient.js

--- backend/models/patient.js
+++ backend/models/patient.js
@@ -43,19 +43,171 @@ module.exports =  class Patient{
         return pool.query(sql, values)
     }
 
-    static add_to_balance(id,amount){
-        const sql = 'UPDATE patient SET balance = balance + $2 where id = $1;'
-        var values = [id,amount]
+    // static add_to_balance(id,amount){
+    //     const sql = 'UPDATE patient SET balance = balance + $2 where id = $1;'
+    //     var values = [id,amount]
 
-        return pool.query(sql, values)
+    //     return pool.query(sql, values)
+    // }
+
+    // static deduct_from_balance(id,amount){
+    //     const sql = 'UPDATE patient SET balance = balance - $2 where id = $1;'
+    //     var values = [id,amount]
+
+    //     return pool.query(sql, values)
+    // }
+
+    static login_to_dashboard(phone,passwd_hash)
+    {
+        const sql = 'select * from patient where phone = $1 and passwd_hash = $2;'
+        var values = [phone,passwd_hash]
+
+        return pool.query(sql,values)
     }
 
-    static deduct_from_balance(id,amount){
-        const sql = 'UPDATE patient SET balance = balance - $2 where id = $1;'
-        var values = [id,amount]
-
-        return pool.query(sql, values)
+    static get_appoint(id)
+    {
+        const sql = 'select * from appointment a inner join doctor d on (a.doctor_id = d.id) where patient_id = $1 order by date_appoint;'
+        var values = [$1]
+        return pool.query(sql,values);
     }
+
+    static get_test(id)
+    {
+        const sql = 'select * from test_appointment a inner join pathologist d on (a.pathologist_id = d.id) inner join test t using (test_id) where patient_id = $1 order by date_appoint;'
+        var values = [$1]
+        return pool.query(sql,values);
+    }
+
+    static get_slot(id)
+    {
+        const sql = 'select * from staff s inner join doctor d using ($1);'
+        var values = [$1]
+        return pool.query(sql,values);
+    }
+
+    static get_slot(id)
+    {
+        const sql = 'select * from staff s inner join doctor d using ($1);'
+        var values = [$1]
+        return pool.query(sql,values);
+    }
+
+    static book_slot(id)
+    {
+        const sql = 'with max_appoints as( \\\
+                select appoints_per_slot from doctor where id = $1), \\\
+                d_slot_name as( \\\
+                select slot_name from staff where id = $1), \\\
+                slots(slot_name, day, start_time, end_time) as( \\\
+                select *  \\\
+                from slot_interval \\\
+                where name = d_slot_name), \\\
+                slot_count(slot_name, day, start_time, count) as( \\\
+                select slot_name, day, start_time , count(*) \\\
+                from appointment \\\
+                where date_appoint >= current_date and time_start > current_time \\\
+                and doctor_id = $1 \\\
+                group by slot_name, day, start_time) \\\
+                \\\
+                select slot_name, day, start_time, end_time, coalesce(count, 0), \\\
+                (case when (day = extract(dow from current_date) and start_time =< current_time)\\\
+                            then current_date + 7\\\
+                     when (day = extract(dow from current_date) and start_time > current_time)\\\
+                            then current_date\\\
+                     else current_date + MOD(day - extract(dow from current_date) + 7, 7)\\\
+                end) as "date"\\\
+                from slots left outer join slot_count using(slot_name, day, start_time)\\\
+                where (count < max_appoints OR count is null)\\\
+                ;'
+        var values = [id]
+
+        return pool.query(sql,values);
+    }
+
+    static make_payement_doc(d_id,day,time,date,p_id,fee)
+    {
+        const sql = 'BEGIN TRANSACTION; \\\
+        with max_appoints as( \\\
+            select appoints_per_slot from doctor where id = $1),\\\
+            d_slot_name as(\\\
+                select slot_name from staff where id = $1),\\\
+            slots(slot_name, day, start_time, end_time) as(\\\
+                select * \\\
+                from slot_interval\\\
+                where name = d_slot_name and day = $2 and start_time = $3 ),\\\
+            slot_count(slot_name, day, start_time, count) as(\\\
+                select slot_name, day, start_time , count(*)\\\
+                from appointment\\\
+                where date_appoint >= current_date and time_start > current_time\\\
+                    and doctor_id = $1 and day = $2 and start_time = $3\\\
+                group by slot_name, day, start_time),\\\
+            cur_appoints as(\\\
+                select coalesce(count, 0)\\\
+                from slots left outer join slot_count using(slot_name, day, start_time))\\\
+                \\\
+                \\\
+                select book_appoint (max_appoints, cur_appoints, date($4), time($3), p_id($5), d_id($1), fee($6));\\\
+        COMMIT TRANSACTION;'
+        var values = [d_id,day,time,date,p_id,fee]
+        
+        return pool.query(sql,values)
+    }
+
+
+    static make_payement_test(d_id,day,time,date,p_id,fee)
+    {
+        const sql = 'BEGIN TRANSACTION; \\\
+        with max_appoints as( \\\
+            select appoints_per_slot from doctor where id = $1),\\\
+            d_slot_name as(\\\
+                select slot_name from staff where id = $1),\\\
+            slots(slot_name, day, start_time, end_time) as(\\\
+                select * \\\
+                from slot_interval\\\
+                where name = d_slot_name and day = $2 and start_time = $3 ),\\\
+            slot_count(slot_name, day, start_time, count) as(\\\
+                select slot_name, day, start_time , count(*)\\\
+                from appointment\\\
+                where date_appoint >= current_date and time_start > current_time\\\
+                    and doctor_id = $1 and day = $2 and start_time = $3\\\
+                group by slot_name, day, start_time),\\\
+            cur_appoints as(\\\
+                select coalesce(count, 0)\\\
+                from slots left outer join slot_count using(slot_name, day, start_time))\\\
+                \\\
+                \\\
+                select book_appoint (max_appoints, cur_appoints, date($4), time($3), p_id($5), d_id($1), fee($6));\\\
+        COMMIT TRANSACTION;'
+        var values = [d_id,day,time,date,p_id,fee]
+        
+        return pool.query(sql,values)
+    }
+
+    static wallet_to_bank(balance,id,acc_id)
+    {
+        const sql = 'BEGIN TRANSACTION; \\\
+        insert into real_transaction(accountant_id, patient_id, amount) values (null, $2, $1);\\\
+        update patient set balance = balance + $1 where id = $2;\\\
+        COMMIT TRANSACTION;\\\
+        '
+        var values = [balance,id,acc_id]
+
+        return pool.query(sql,values)
+    }
+
+    static deduct_from_wallet(amount,id)
+    {
+        const sql = 'BEGIN TRANSACTION;\\\
+        insert into real_transaction(accountant_id, patient_id, amount) values (null, $2, $1);\\\
+        update patient set balance = balance - $1 where id = $2;\\\
+        COMMIT TRANSACTION;\\\
+        '
+        var values = [amount,id]
+
+        return pool.query(sql,values)
+    }
+
 
     // static get_patient_info(id){
     //     const sql = 'SELECT * from patient where id = $1;'
